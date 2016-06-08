dd6ae86452d9b8a704573ac45994d1a64461c99a
Kjetil <kjetil.mjos@gmail.com>
2016-06-08 14:52:25 +0200

    Create routes.js

---
+++ app/routes.js
@@ -0,0 +1,9 @@
+var models = require('./models/schemas');
+module.exports = function(app) {
+
+
+  // normal routes ===============================================================
+  // show the home page (will also have our login links)
+  app.get('/', function(req, res) {
+    res.render('index.ejs');
+  });
