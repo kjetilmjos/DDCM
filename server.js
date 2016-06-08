0a16a2c14f6c05e326021fa91be8fbdacdb60462
Kjetil <kjetil.mjos@gmail.com>
2016-06-08 14:51:28 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 80;
+var mongoose = require('mongoose');
+var config = require('./config/config.json');
+
+var morgan       = require('morgan');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+app.use('/static', express.static('public'));
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+
+// routes ======================================================================
+require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
