11ca32692769b831311ab7b3a47aa436fe365cdb
Kjetil <kjetil.mjos@gmail.com>
2016-06-08 15:46:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,22 +7,20 @@ var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 80;
 var mongoose = require('mongoose');
-var config = require('./config/config.json');
 
 var morgan       = require('morgan');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-var configDB = require('./config/database.js');
+//var configDB = require('./config/database.js');
 
 app.use('/static', express.static('public'));
 
 // configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+//mongoose.connect(configDB.url); // connect to our database
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
