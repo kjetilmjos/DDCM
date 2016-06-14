de11828c4c0497df0c2fe0f832761b7f4a6d2269
Kjetil <kjetil.mjos@gmail.com>
2016-06-14 14:21:54 +0200

    Add files via upload

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,9 @@ module.exports = function(app) {
 
 
     var multer = require('multer');
-    var upload = multer({
-        dest: 'upload/'
-    });
+    //    var upload = multer({
+    //      dest: 'upload/'
+    //  });
     var storage = multer.diskStorage({
         destination: function(req, file, cb) {
             cb(null, 'upload/')
@@ -14,9 +14,20 @@ module.exports = function(app) {
             cb(null, "excel_export" + '.xlsx') //Appending .xlsx
         }
     })
+    var storage_config = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, 'upload/')
+        },
+        filename: function(req, file, cb) {
+            cb(null, "CONFIG_query" + '.json') //Appending .json
+        }
+    })
 
     var upload = multer({
         storage: storage
+    });
+    var upload_config = multer({
+        storage: storage_config
     });
     // normal routes ===============================================================
     // show the home page (will also have our login links)
@@ -30,16 +41,62 @@ module.exports = function(app) {
         console.log(req.file); //form files
         var PythonShell = require('python-shell');
         var pyshell = new PythonShell('./python/file_validation.py');
-        pyshell.on('message', function (message) {
-  // received a message sent from the Python script (a simple "print" statement)
-  console.log(message);
-});
+        var mssg = "";
+        pyshell.on('message', function(message) {
+            // received a message sent from the Python script (a simple "print" statement)
+            console.log(message);
+            mssg = message;
+        });
 
-// end the input stream and allow the process to exit
-pyshell.end(function (err) {
-  if (err) throw err;
-  console.log('finished');
-});
+        // end the input stream and allow the process to exit
+        pyshell.end(function(err) {
+            if (err) throw err;
+            console.log('finished');
+            res.send(mssg);
+        });
+
+    })
+    app.post('/config_upload', upload_config.single('config_file'), function(req, res) {
+        // req.file is the `avatar` file
+        // req.body will hold the text fields, if there were any
+        console.log(req.file); //form files
+        var PythonShell = require('python-shell');
+        var pyshell = new PythonShell('./python/mongodb_query.py');
+        var msg = "";
+        pyshell.on('message', function(message) {
+            // received a message sent from the Python script (a simple "print" statement)
+            console.log(message);
+            msg = message;
+        });
+        // end the input stream and allow the process to exit
+        pyshell.end(function(err) {
+            if (err) throw err;
+            console.log('finished');
+            res.send(msg);
+        });
+
+
+    })
+
+    app.post('/cleanup_files', function(req, res) {
+        // req.file is the `avatar` file
+        // req.body will hold the text fields, if there were any
+
+        var PythonShell = require('python-shell');
+        var pyshell = new PythonShell('./python/cleanup.py');
+        var msgg = "";
+        pyshell.on('message', function(message) {
+            // received a message sent from the Python script (a simple "print" statement)
+            console.log(message);
+            msgg = message;
+        });
+        // end the input stream and allow the process to exit
+        pyshell.end(function(err) {
+            if (err) throw err;
+            console.log('finished');
+            res.send(msgg);
+        });
+
 
     })
 }
